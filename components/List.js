3142de99458bcbc5cab1cda8a63e57de69694100
Maria <mk.sillanmaki@gmail.com>
2019-09-02 09:26:04 +0300

    Fetch data from json
    This is fetching data with AJAX task A.

--- components/List.js
+++ components/List.js
@@ -1,12 +1,29 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState, useEffect} from 'react';
 import {FlatList} from 'react-native';
 import ListItem from './ListItem';
 import {MediaContext} from '../contexts/MediaContext';
 
-const List = () => {
-  // eslint-disable-next-line no-unused-vars
+const useFetch = (url) => {
   const [media, setMedia] = useContext(MediaContext);
-  console.log(media);
+  const [loading, setLoading] = useState(true);
+  // const fetchUrl = async () => {
+  async function fetchUrl() {
+    const response = await fetch(url);
+    const json = await response.json();
+    setMedia(json);
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    fetchUrl();
+  }, []);
+  return [media, loading];
+};
+
+const List = () => {
+  const [media, loading] = useFetch('https://raw.githubusercontent.com/mattpe/wbma/master/docs/assets/test.json');
+  console.log(loading);
+  console.log('media', media);
   return (
     <FlatList
       data={media}
